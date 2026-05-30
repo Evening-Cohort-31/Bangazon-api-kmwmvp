0eb57c74720676caa13d901141f7b3c5e2be7a05
Lea Edwards <leaxedw@gmail.com>
2026-05-30 12:20:30 -0500

    added new url that generates report for items  or less

--- bangazon/urls.py
+++ bangazon/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from bangazonapi.models import *
 from bangazonapi.views import *
-from bangazonapi.views.report import completed_orders_report, pending_orders_report
+from bangazonapi.views.report import completed_orders_report, pending_orders_report, inexpensive_products_report
 
 # pylint: disable=invalid-name
 router = routers.DefaultRouter(trailing_slash=False)
@@ -39,5 +39,10 @@ urlpatterns = [
         pending_orders_report,
         name="pending_orders"
 
+    ),
+    path(
+        "reports/inexpensive_products",
+        inexpensive_products_report,
+        name="inexpensive_products"
     )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
